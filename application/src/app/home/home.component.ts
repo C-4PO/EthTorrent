4be335a5f0a0417c99366839c14ecfad7a6439d4
Emma Hsueh <ehsueh1993@gmail.com>
2019-09-07 10:15:05 -0400

    Create wallet functionality

--- application/src/app/home/home.component.ts
+++ application/src/app/home/home.component.ts
@@ -38,4 +38,27 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  createWallet() {
+    // open squarelink api call in a new window
+    const clientId = "c76d2361577b6d10fc12"
+    const redirectUrl = "http://localhost:4200/"
+    const url = "https://app.squarelink.com/authorize?client_id=" + clientId + "&scope=[user,wallets:admin]&redirect_uri="+ redirectUrl + "&response_type=token"
+    window.location.replace(url)
+    
+    const token = window.location.href.split("?")[1].split("=")[1].split("&")[0]
+    
+    const axios = require('axios')
+    axios.post('https://api.squarelink.com/wallet',{
+      access_token: token
+    }).then((res) => {
+      console.log(res)
+      const getBalanceUrl = "http://api.ethplorer.io/getAddressInfo/" + res.address + "?apiKey=freekey"
+      // {"address":"0x9afcddf5ade14065a491917a6d9e8fe94121c98e","ETH":{"balance":0},"countTxs":0}
+      window.open(getBalanceUrl, '_blank')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
+  }
 }
